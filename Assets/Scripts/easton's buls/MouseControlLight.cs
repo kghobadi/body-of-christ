86c4dc1511c5f5f1bf427631099f71a895bb0aab
Easton <eastonself@gmail.com>
2020-07-02 15:09:36 -0400

    auraaaaa

---
+++ Assets/Scripts/easton's buls/MouseControlLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControlLight : MonoBehaviour
+{
+    private Light lit;
+    public float spotAng, spotAngOrg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lit = GetComponent<Light>();
+        spotAng = lit.range;
+        spotAngOrg = spotAng;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetMouseButton(0))
+        {
+            spotAng = Mathf.Lerp(spotAng, 10, Time.deltaTime);
+        }
+        else
+        {
+            spotAng = Mathf.Lerp(spotAng, spotAngOrg, Time.deltaTime);
+        }
+
+        lit.range = spotAng;
+    }
+}
