705b70fa821df805d03db6969d2a4bd01ce383e9
kghobadi <kasghobadi@gmail.com>
2021-01-18 17:24:26 -0500

    added some dialogue audio, fixed up riding to the  canyon, got text illumination mostly working :)

---
+++ Assets/Scripts/Lighting/ControlLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//class for controlling light movement with mouse movement 
+public class ControlLight : MonoBehaviour
+{
+    public Material lightAng;
+    float mouseMovement;
+
+    void Update()
+    {
+        mouseMovement += Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y"));
+        transform.localEulerAngles += new Vector3(0, 5f * Input.GetAxis("Mouse X"), 0);
+        lightAng.SetFloat("_MouseInput", mouseMovement);
+    }
+}
--- Assets/Scripts/easton's buls/randomRotation.cs
+++ Assets/Scripts/easton's buls/randomRotation.cs
@@ -6,29 +6,25 @@ public class randomRotation : MonoBehaviour
 {
     float timer;
     public Material lightAng;
-    float mouseMovement;
     float speed;
-    // Start is called before the first frame update
+
     void Start()
     {
         timer = .5f;
         speed = 1;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(timer < 0)
         {
-            //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Random.RandomRange(0, 360), 0);
-            //timer = Random.Range(0.5f, 1.5f);
-            //speed = Random.Range(-2f, 2f);
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Random.Range(0, 360), 0);
+            timer = Random.Range(0.5f, 1.5f);
+            speed = Random.Range(-2f, 2f);
         }
-        mouseMovement += Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y"));
-        transform.localEulerAngles += new Vector3(0, 5f*Input.GetAxis("Mouse X"), 0);
-        //transform.localPosition += new Vector3(0, -50f * Input.GetAxis("Mouse Y") * Time.deltaTime, 0);
-        //lightAng.SetVector("_LightDir", transform.localEulerAngles);
-        lightAng.SetFloat("_MouseInput", mouseMovement);
-        //timer -= Time.deltaTime;
+
+        transform.localPosition += new Vector3(0, -50f * Input.GetAxis("Mouse Y") * Time.deltaTime, 0);
+        lightAng.SetVector("_LightDir", transform.localEulerAngles);
+        timer -= Time.deltaTime;
     }
 }
