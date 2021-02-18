482befb85274f8fe8064b512901a24dd70a8b4b7
kghobadi <kasghobadi@gmail.com>
2021-02-17 22:16:11 -0500

    lots of visual updates

--- Assets/Scripts/Lighting/ControlLight.cs
+++ Assets/Scripts/Lighting/ControlLight.cs
@@ -1,17 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 //class for controlling light movement with mouse movement 
 public class ControlLight : MonoBehaviour
 {
+    //not using this anymore 
     public Material lightAng;
-    float mouseMovement;
+
+    //cam refs
+    private Camera mainCam;
+    private Transform camTransform;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+        camTransform = mainCam.transform;
+    }
 
     void Update()
     {
-        mouseMovement += Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y"));
-        transform.localEulerAngles += new Vector3(0, 5f * Input.GetAxis("Mouse X"), 0);
-        lightAng.SetFloat("_MouseInput", mouseMovement);
+        //when left click
+        if (Input.GetMouseButton(0))
+        {
+            transform.localEulerAngles = camTransform.localEulerAngles;
+        }
+        
+        //mouseMovement += Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y"));
+        //transform.localEulerAngles += new Vector3(0, 5f * Input.GetAxis("Mouse X"), 0);
+        
     }
 }
